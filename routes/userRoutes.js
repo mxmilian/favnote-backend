1c0f2a4740d82b134e8fb3e4993480efa711eea6
s450026 <maksymilian.kierski@gmail.com>
2020-06-30 18:48:46 +0200

    Add routes for signIn and logOut

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,7 +1,9 @@
 const router = require('express').Router();
 
-const { signUp } = require('../controllers/usersControllers');
+const { signUp, signIn, logOut } = require('../controllers/usersControllers');
 
-router.route('/').post(signUp);
+router.route('/signup').post(signUp);
+router.route('/signin').post(signIn);
+router.route('/logout').post(logOut);
 
 module.exports = router;
