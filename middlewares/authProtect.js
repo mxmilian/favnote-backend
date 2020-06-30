b3f12f8157459c08f9ce19c2f4d5f60090a24f77
s450026 <maksymilian.kierski@gmail.com>
2020-06-30 19:36:59 +0200

    Add isCurrentlyLogIn auth protection

--- middlewares/authProtect.js
+++ middlewares/authProtect.js
@@ -44,7 +44,34 @@ const restrictRoute = (...roles) => {
   };
 };
 
+const isCurrentlySignIn = async (req, res, next) => {
+  if (req.cookies.jwt) {
+    try {
+      // 1) Verify token
+      const decodedToken = await promisify(jwt.verify)(req.cookies.jwt, process.env.JWT_SECRET);
+
+      // 2) Check if user still exists
+      const currentUser = await Users.findById(decodedToken.id);
+      if (!currentUser) return next();
+
+      // 3) Check if user changed password after the token was issued
+      if (isUserExists.passwordChangedAt) {
+        const changedTimestamp = parseInt(isUserExists.passwordChangedAt.getTime() / 1000, 10);
+        if (changedTimestamp > decodedToken.iat) return next('Password was changed!');
+      }
+
+      // THERE IS A LOGGED IN USER
+      res.locals.user = currentUser;
+      return next();
+    } catch (err) {
+      return next();
+    }
+  }
+  next();
+};
+
 module.exports = {
   protectRoute,
-  restrictRoute
+  restrictRoute,
+  isCurrentlySignIn
 };
