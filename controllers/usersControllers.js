dc4ea438d3c4911f8aeacf6916d5d572d9b9c467
s450026 <maksymilian.kierski@gmail.com>
2020-06-30 18:43:06 +0200

    Add controller for signIn and logOut

--- controllers/usersControllers.js
+++ controllers/usersControllers.js
@@ -1,7 +1,6 @@
 const Users = require('../models/usersModels');
 const jwt = require('jsonwebtoken');
-//const bcrypt = require('bcryptjs');
-//const crypto = require('crypto');
+const bcrypt = require('bcryptjs');
 const catchAsync = require('../errors/catchAsync');
 
 const signToken = (id) =>
@@ -9,7 +8,7 @@ const signToken = (id) =>
     expiresIn: process.env.JWT_EXPIRES_IN,
   });
 
-const createSendToken = (user, statusCode, res) => {
+const createSendToken = (user, statusCode, res, message) => {
   const token = signToken(user._id);
   res.cookie('jwt', token, {
     expires: new Date(Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000),
@@ -21,6 +20,7 @@ const createSendToken = (user, statusCode, res) => {
     status: 'success',
     data: {
       token,
+      message,
       user,
     },
   });
@@ -35,7 +35,29 @@ const signUp = catchAsync(async (req, res, next) => {
     confirmPassword,
   });
   await newUser.save({ validateBeforeSave: false });
-  createSendToken(newUser, 201, res);
+  createSendToken(newUser, 201, res, 'Account created successfully');
 });
 
-module.exports = { signUp };
+const signIn = catchAsync(async (req, res, next) => {
+  const { name, email, password } = req.body;
+
+  if ((!name && !email) || !password) return next();
+
+  const user = name
+    ? await Users.findOne({ name }).select('+password')
+    : await Users.findOne({ email }).select('+password');
+
+  if (!user || !(await bcrypt.compare(password, user.password))) return next();
+
+  createSendToken(user, 200, res, 'Logged successfully');
+});
+
+const logOut = catchAsync(async (req, res, next) => {
+  res.cookie('jwt', 'logout', {
+    expires: new Date(Date.now() + 10 * 1000),
+    httpOnly: true,
+  });
+  res.status(200).json({ status: 'success' });
+});
+
+module.exports = { signUp, signIn, logOut };
