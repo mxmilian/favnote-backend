7e5d1b035f8e78e0bd5d91a840052e203f39601d
s450026 <maksymilian.kierski@gmail.com>
2020-08-19 22:35:51 +0200

    Set the configuration of multer

--- utils/mutlerConf.js
+++ utils/mutlerConf.js
@@ -1,6 +1,26 @@
 const multer = require('multer');
+const Errors = require('../errors/Errors');
 
-const upload = multer({ dest: 'public/image/users' });
+const multerStorage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, 'public/image/users');
+  },
+  filename: (req, file, cb) => {
+    const extensions = file.mimetype.split('/')[1];
+    //USER ID SHOULD CHANGE IN FUTURE!
+    cb(null, `user-${process.env.USERID}-${Date.now()}.${extensions}`);
+  },
+});
+
+const multerFilter = (req, file, cb) => {
+  if (file.mimetype.startsWith('image')) {
+    cb(null, true);
+  } else {
+    cb(new Errors('Only images can be uploaded', 400), false);
+  }
+};
+
+const upload = multer({ storage: multerStorage, fileFilter: multerFilter });
 
 const uploadUserPhoto = upload.single('photo');
 
